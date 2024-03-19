5762ee08c2a019a3c7e9ab3968b9387b0ae58f24
Atith794 <68183452+Atith794@users.noreply.github.com>
2024-03-19 22:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+let btn = document.getElementById("swap");
+let maini = document.getElementById("app");
+btn.addEventListener("click", handleClick);
 
+function handleClick(){
+	maini.classList.toggle("night");
+}
