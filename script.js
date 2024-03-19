94e5f7e8d769a11c693f388b332da9e150a57370
Atith794 <68183452+Atith794@users.noreply.github.com>
2024-03-19 22:22:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //your code here
+// let btn = document.getElementById("swap");
+// let maini = document.getElementById("app");
+// btn.addEventListener("click", handleClick);
+
+// function handleClick(){
+// 	maini.classList.toggle("night");
+// }
+
+
+
 let btn = document.getElementById("swap");
-let maini = document.getElementById("app");
 btn.addEventListener("click", handleClick);
 
 function handleClick(){
-	maini.classList.toggle("night");
+  btn.classList.toggle("button_night"); // Toggle the class on the button itself
 }
